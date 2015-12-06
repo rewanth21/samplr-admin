804980cadc834c53392eb0eceb48f2706fdd31c8
Kaelan Cooter <dcgamers@gmail.com>
2015-12-05 22:43:51 -0500

    added breadcrumps

--- src/containers/GroupSurveysContainer.js
+++ src/containers/GroupSurveysContainer.js
@@ -4,7 +4,8 @@ import * as actionCreators from '../action-creators';
 import { bindActionCreators } from 'redux';
 import { connect } from 'react-redux';
 import _ from 'lodash';
-import { Panel } from 'react-bootstrap';
+import { Panel, Breadcrumb, BreadcrumbItem } from 'react-bootstrap';
+import { LinkContainer } from 'react-router-bootstrap';
 
 function select(state) {
     return {
@@ -17,12 +18,20 @@ class GroupSurveysContainer extends Component {
     render() {
         const { dispatch, groups, surveys } = this.props;
         const groupId = this.props.routeParams.id
-        const group = _.findWhere(groups.list, { id: groupId});
-        console.log(group);
 
         return (
             <div className="container">
                 <Panel>
+                    <Breadcrumb>
+                        <LinkContainer to="groups">
+                            <BreadcrumbItem>
+                                Groups
+                            </BreadcrumbItem>
+                        </LinkContainer>
+                        <BreadcrumbItem active>
+                            {groupId}
+                        </BreadcrumbItem>
+                    </Breadcrumb>
                     <h1>Surveys in group <i>{groupId}</i></h1>
                     <SurveyList groupId={groupId} surveys={surveys}
                         {...bindActionCreators(actionCreators, dispatch)} />
